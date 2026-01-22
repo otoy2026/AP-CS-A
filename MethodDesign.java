4ed57473ce292fc5b39201fe84fdfdc986b04775
Owen Toy <otoy2026@cchsdons.com>
2026-01-22 13:24:06 -0800

    Add files via upload

---
+++ MethodDesign.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.methoddesign;
+
+/**
+ *
+ * @author OToy2026
+ */
+public class MethodDesign {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
