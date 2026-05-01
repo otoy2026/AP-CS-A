5bf63c8aa781cd6c647cd72b8e312353823aa0ba
Owen Toy <otoy2026@cchsdons.com>
2026-05-01 14:10:23 -0700

    Add files via upload

--- FlightSim.java
+++ FlightSim.java
@@ -26,14 +26,14 @@ public class FlightSim {
       
         System.out.println("Welcome to Flight Simulator.");
         
-        Path path = Paths.get("Info", "flights.txt");
+        Path path1 = Paths.get("Info", "flights.txt");
         ArrayList<flightPath> flights = new ArrayList<>();
         
-        if (Files.exists(path)){
+        if (Files.exists(path1)){
         try  {
-            Scanner scanner = new Scanner(path);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+            Scanner scan1 = new Scanner(path1);
+            while (scan1.hasNextLine()) {
+                String line = scan1.nextLine();
                 String[] parts = line.split(",");
                 
                 String company = parts[0];
@@ -51,9 +51,78 @@ public class FlightSim {
         }
         
         Random rand = new Random();
-        int randomNum = rand.nextInt(flights.size());
-        System.out.println(flights.get(randomNum));
+        int randomFlight = rand.nextInt(flights.size());
+        System.out.println(flights.get(randomFlight));
         
+        //-----------------------------------------------------------------
+        
+        Path path2 = Paths.get("Info", "prompts.txt");
+        ArrayList<promptFormat> prompts = new ArrayList<>();
+        
+        if (Files.exists(path2)) {
+            try (Scanner scan2 = new Scanner(path2)) {
+                scan2.useDelimiter("-"); 
+                while (scan2.hasNext()) {
+                    String content = scan2.next().trim();
+                    if (!content.isEmpty()) {
+                    prompts.add(new promptFormat(content));
+                    }
+                }
+        
+    } catch (IOException e) {
+        System.err.println("Error reading prompts: " + e.getMessage());
+    }
+}
+        
+        //-----------------------------------------------------------------
+        
+        System.out.println(prompts.get(0));
+        Scanner input = new Scanner(System.in);
+        int choice1 = input.nextInt();
+        if(choice1==1){
+            System.out.println(prompts.get(1));
+        }else{
+            System.out.println(prompts.get(2));
+        }
+        int rand1 = rand.nextInt(2);
+        if(rand1==1){
+            System.out.println(prompts.get(3));
+            int choice2 = input.nextInt();
+            if(choice2==1){
+                System.out.println(prompts.get(5));
+                int choice3 = input.nextInt();
+                if (choice3==1){
+                    System.out.println(prompts.get(6));
+                    int answer = input.nextInt();
+                    if (answer==132){
+                        System.out.println(prompts.get(7));
+                        System.out.println(prompts.get(9));
+                    }
+                }           
+            }else if(choice2==2){
+                    
+            }else{
+                System.out.println("Invalid input.");
+            }
+        }else{
+            System.out.println(prompts.get(4));
+            int choice2 = input.nextInt();
+            if(choice2==1){
+                System.out.println(prompts.get(6));
+                int answer = input.nextInt();
+                if (answer==132){
+                    System.out.println(prompts.get(7));
+                    System.out.println(prompts.get(9));
+                }else{
+                    System.out.println(prompts.get(8));
+                    System.out.println(prompts.get(9));
+                }
+            }else if(choice2==2){
+                System.out.println(prompts.get(9));
+            }else{
+                System.out.println("Invalid input.");
+            }
+        }      
     }
 }
 
@@ -74,7 +143,7 @@ class flightPath{
         miles = inMiles;
         elapsed = inElapsed;
         //create string to return
-        this.flightSum = "Today you will be flying a "+company+" "+aircraft+" from "+startPoint+" to "+endPoint+". The trip will cover "+miles+" miles and take an estimated "+elapsed+" minutes.";    
+        this.flightSum = "\nToday you will be flying a "+company+" "+aircraft+" from "+startPoint+" to "+endPoint+". The trip will cover "+miles+" miles and take an estimated "+elapsed+" minutes.";    
     }
     
     //return flightSum; 
@@ -82,6 +151,28 @@ class flightPath{
         public String toString() {
             return flightSum;
     }
+}
+
+//-----------------------------------------------------------------
+
+class promptFormat{
+    
+    //declare variables
+    private String textBody;
+    private String[] newText;
+    
+    public promptFormat(String inText){
+        textBody = inText;
+        newText = textBody.split("\\~");
+        
+    }
+    
+    //return text
+    @Override
+        public String toString() {
+            return String.join("\n", newText);
+    }
+    
 }
 
 //-----------------------------------------------------------------
